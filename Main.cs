d121ec9e4efc53b1d344636e05d00301050e95b9
phasic22 <65420014+phasic22@users.noreply.github.com>
2020-05-17 16:02:32 -0700

    0.1.1
    // 0.1.1
    // added in-game report for when using/not using siege weapon via distance
    // fixed log to account for different output paths
    // fixed order logic to better reflect user formation orders

--- Main.cs
+++ Main.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Linq;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 using HarmonyLib;
-using TaleWorlds.Engine;
 using TaleWorlds.Library;
 using System.IO;
-using System.Linq.Expressions;
 using TaleWorlds.MountAndBlade.Missions.Handlers;
-using System.Diagnostics;
-using System.Runtime;
-using System.Threading;
+using TaleWorlds.MountAndBlade.ViewModelCollection;
 
 namespace FixSiegeAI
 {
@@ -18,13 +13,16 @@ namespace FixSiegeAI
 	public class Main : MBSubModuleBase
 	{
 		// Print to log in-game and desktop
-		public static void Log(string s)
+		public static void Log(string s, bool ingameonly = false)
 		{
-			bool debugging = true; // change this to true for helpful debugging info in-game and to log below
-			if (debugging)
+			bool debugging = false; // change this to true for helpful debugging info in-game and to log below
+			if (ingameonly || debugging)
 			{
 				InformationManager.DisplayMessage(new InformationMessage(s));
-				string log_path = @"C:\Users\Shadow\Desktop\Log.txt"; // change where you want the log to save
+			}
+			if (debugging)
+			{
+				string log_path = @"C:\Users\Shadow\Desktop\FixSiegeAI_Log.txt"; // change where you want the log to save
 				try
 				{
 					using (System.IO.StreamWriter file = new System.IO.StreamWriter(log_path, true))
@@ -37,7 +35,7 @@ namespace FixSiegeAI
 		protected override void OnBeforeInitialModuleScreenSetAsRoot()
 		{
 			try { File.Delete(@"C:\Users\Shadow\Desktop\Log.txt"); } catch { };
-			InformationManager.DisplayMessage(new InformationMessage("FixSiegeAI module loaded."));
+			Main.Log("FixSiegeAI module loaded.", true);
 			var harmony = new Harmony("fixsiegeai");
 			harmony.PatchAll();
 			Main.Log("Harmony patches loaded.");
@@ -52,7 +50,18 @@ namespace FixSiegeAI
 				if (sw.Side == Mission.Current.PlayerTeam.Side)
 				{
 					sw.ForcedUse = false;
-					Main.Log(sw.Side.ToString()+" "+sw.GetSiegeEngineType().ToString() + " forced use haulted."); 
+					Main.Log(sw.Side.ToString() + " " + sw.GetSiegeEngineType().ToString() + " forced use haulted.");
+					// TODO: attempt to fix siege angle restrictions
+					//if (sw is RangedSiegeWeapon)
+					//{
+					//	Main.Log(sw.Side.ToString() + " " + sw.GetSiegeEngineType().ToString() + " release angles: " + (sw as RangedSiegeWeapon).TopReleaseAngleRestriction.ToString());
+					//	Main.Log(sw.Side.ToString() + " " + sw.GetSiegeEngineType().ToString() + " release angles: " + (sw as RangedSiegeWeapon).BottomReleaseAngleRestriction.ToString());
+					//	if (sw is Trebuchet)
+					//	{
+					//		(sw as RangedSiegeWeapon).BottomReleaseAngleRestriction = 0.1f;
+					//	}
+					//	Main.Log(sw.Side.ToString() + " " + sw.GetSiegeEngineType().ToString() + " release angles widened.");
+					//}
 				}
 			}
 		}
@@ -121,39 +130,37 @@ namespace FixSiegeAI
 				Main.Log("Is formation AI controlled?: " + formation.IsAIControlled.ToString());
 				if (isAIC && is_dep) { Main.Log("Order blocked."); return false; } // prevents any ai movement orders during deployment
 				if (is_dep) { return true; } // allows using siege engines during deployment
-				var fo_loc = formation.MovementOrder.GetPosition(formation).AsVec2; // get where they're told to go
+				Vec2 fo_loc = formation.MovementOrder.GetPosition(formation).AsVec2; // get where they're told to go
 				Main.Log("Order Location: " + fo_loc.ToString());
 				if (formation.MovementOrder.OrderType == OrderType.StandYourGround) { Main.Log("Stop order issued"); }
 				foreach (SiegeWeapon sw in Mission.Current.ActiveMissionObjects.FindAllWithType<SiegeWeapon>())
 				{
 					if (!isAIC && sw.Side == Mission.Current.PlayerTeam.Side && !is_dep) // only works when player gives order for player's side's siegeweapon
 					{
-						var sw_loc = sw.GameEntity.GlobalPosition.AsVec2;
-						double distance = sw_loc.Distance(fo_loc);
+						Vec2 sw_loc = sw.GameEntity.GlobalPosition.AsVec2;
+						float distance = sw_loc.Distance(fo_loc);
 						Main.Log("Checking distance to: " + sw.GetSiegeEngineType().ToString());
 						Main.Log("Siege Weapon Location: " + sw_loc.ToString());
 						Main.Log("Distance: " + distance.ToString());
 						if (formation.MovementOrder.OrderType == OrderType.StandYourGround && formation.IsUsingMachine(sw)) 
 						{
 							formation.StopUsingMachine(sw, true);
-							Main.Log("Formation stopping using: " + sw.GetSiegeEngineType().ToString());
-							InformationManager.DisplayMessage(new InformationMessage("Formation stopping using machine: " + sw.GetSiegeEngineType().ToString()));
+							Main.Log("Formation stopping using machine: " + sw.GetSiegeEngineType().ToString(), true);
 						}
-						if (distance <= 10 && !formation.IsUsingMachine(sw))
+						if (distance <= 15 && !formation.IsUsingMachine(sw))
 						{
 							formation.StartUsingMachine(sw, true);
-							Main.Log("Formation starting using new machine: " + sw.GetSiegeEngineType().ToString());
-							InformationManager.DisplayMessage(new InformationMessage("Formation starting using new machine: " + sw.GetSiegeEngineType().ToString()));
+							Main.Log("Formation starting using new machine: " + sw.GetSiegeEngineType().ToString(), true);
 						}
-						else if (distance > 10 && formation.IsUsingMachine(sw))
+						if (distance <= 15 && formation.IsUsingMachine(sw))
+						{
+							formation.StartUsingMachine(sw, true);
+							Main.Log("Formation continuing to use machine: " + sw.GetSiegeEngineType().ToString(), true);
+						}
+						if (distance > 15 && formation.IsUsingMachine(sw))
 						{
 							formation.StopUsingMachine(sw, true);
-							Main.Log("Formation stopping using: " + sw.GetSiegeEngineType().ToString());
-							InformationManager.DisplayMessage(new InformationMessage("Formation stopping using machine: " + sw.GetSiegeEngineType().ToString()));
-						}
-						else
-						{
-							Main.Log("Same as previous order");
+							Main.Log("Formation stopping using machine: " + sw.GetSiegeEngineType().ToString(), true);
 						}
 					}
 				}
@@ -164,8 +171,16 @@ namespace FixSiegeAI
 }
 
 // 0.1.1
-// added report for when using/not using siege weapon
-// fixed log to account for different paths
+// added in-game report for when using/not using siege weapon via distance
+// fixed log to account for different output paths
+// fixed order logic to better reflect user formation orders
+
+// new todo
+// have troops attack/defend paths of least resistance
+// give a report when using the blue-gear / grey-gear command
+// fix so ram is deactivated/disabled to no longer give blue-gear icon after first gate is down
+// first so grey-gear inner gate works from outside walls
+// add settings menu to enable/disable features
 
 // breaks
 // if (sw.Users != null) { formation.MovementOrder = MovementOrder.MovementOrderFollow(sw.Users.First()); }
